0e335f0e36b8f00d9d3793c78e1f402b08f7f37c
joaopamarall <joaoamaral56@hotmail.com>
2023-06-08 22:21:34 -0300

    Iniciando a conexão com banco de dados

---
+++ PHP/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="../images/logo2.png" type="image/x-icon">
+	<link rel="stylesheet" href="../styles/style.css">
+	<title>TECGAMES</title>
+</head>
+<body>
+	<header class="cabecalho">
+		<nav class="menu">
+			<div class="logo">
+				<img src="../images/logo2.png" class="image-logo">
+				<a class="store-name" href="../PHP/inicio.php">TECGAMES</a>
+			</div>
+			<div class="mobile-menu">
+			  <div class="line1"></div>
+			  <div class="line2"></div>
+			  <div class="line3"></div>
+			</div>
+			<ul class="nav-list">
+				<li><a href="Lancamento.php">Lançamento</a></li>
+				<li><a href="PcGamer.php">PC Gamer</a></li>
+				<li><a href="Perifericos.php">Periféricos</a></li>
+				<li><a href="OfertaDoDia.php">Oferta do dia</a></li>
+				<li><a href="login.php">Login/Cadastro</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<main class="conteudo">
+
+    <div class="card-login">
+        <h1>Login</h1>
+        <p>Coloque seu email:</p>
+        <input type="mailto" value="example@example.com">
+        <input type="button" value="Enviar">
+    </div>
+    <div class="card-cadastro">
+        <h1>Cadastre-se</h1>
+        <p>Cadastre seu email:</p>
+        <input type="mailto" value="example@example.com">
+        <input type="button" value="Enviar">
+    </div>
+		
+	</main>
+	<footer>
+			<p class="rodape">TECGAMES &copy; 2023</p>
+	</footer>
+	<script src="../javascript/script.js"></script>
+</body>
+</html>
